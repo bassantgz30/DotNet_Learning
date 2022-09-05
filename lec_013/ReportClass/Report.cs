06d989cfdf3a35edbf0d7717214f5a902ac2333b
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-05 10:32:23 +0200

    Added Delegate

--- lec_013/ReportClass/Report.cs
+++ lec_013/ReportClass/Report.cs
@@ -1,44 +1,23 @@
-﻿using System;
+using System;
 using EmployeesClass;
 
 namespace ReportClass
 {
     public class Report
     {
-        public void ProcessEmployee_60000P(Employee[] employees)
+        // Create a delegate to handel different conditions of reports
+        // Delegate is a method but witgout a body.
+        public delegate bool EmployeeFilter(Employee e);
+
+        // Creat one general method for reports
+        public void ProcessEmployee(Employee[] employees, string reportTitle, EmployeeFilter empFilter)
         {
-            Console.WriteLine("Employees with sales more than or equal 60,000");
+            Console.WriteLine(reportTitle);
             Console.WriteLine("===================================");
 
             foreach(var e in employees)
             {
-                if(e.Sales >= 60000)
-                {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
-            }
-            Console.WriteLine("\n\n");
-        }
-
-        public void ProcessEmployee_30000_60000(Employee[] employees)
-        {
-            Console.WriteLine("Employees with Sales between 30,000 and 60,000 plus sales.");
-            Console.WriteLine("===================================");
-
-            foreach(var e in employees)
-            {
-                if((e.Sales >= 30000) && (e.Sales < 60000))
-                {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
-            }
-            Console.WriteLine("\n\n");
-        }
-
-        public void ProcessEmployee_30000L(Employee[] employees)
-        {
-            Console.WriteLine("Employees with Sales Less than 30,000.");
-            Console.WriteLine("===================================");
-
-            foreach(var e in employees)
-            {
-                if(e.Sales < 30000)
+                if(empFilter(e))
                 {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
             }
             Console.WriteLine("\n\n");
