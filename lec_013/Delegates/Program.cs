c71db692f28cd4916e25bca671e9de570c3476f6
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-05 10:07:38 +0200

    Add files via upload
    Initial design

---
+++ lec_013/Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using EmployeesClass;
+using ReportClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var emps = new Employee[]
+        {
+            new Employee {Id=10, Name="Nada", Sales=145478, Gender="Female"},
+            new Employee {Id=11, Name="Rami", Sales=12347, Gender="Male"},
+            new Employee {Id=12, Name="Roka", Sales=60478, Gender="Female"},
+            new Employee {Id=13, Name="Mark", Sales=30047, Gender="Male"},
+            new Employee {Id=14, Name="Jack", Sales=40478, Gender="Male"},
+            new Employee {Id=15, Name="Rasha", Sales=50347, Gender="Female"},
+            new Employee {Id=16, Name="Saja", Sales=70478, Gender="Female"},
+            new Employee {Id=17, Name="Toni", Sales=20047, Gender="Male"}
+        };
+
+        Report report1 = new Report();
+        report1.ProcessEmployee_60000P(emps);
+        report1.ProcessEmployee_30000_60000(emps);
+        report1.ProcessEmployee_30000L(emps);
+    }
+}
---
+++ lec_013/ReportClass/Report.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using EmployeesClass;
+
+namespace ReportClass
+{
+    public class Report
+    {
+        public void ProcessEmployee_60000P(Employee[] employees)
+        {
+            Console.WriteLine("Employees with sales more than or equal 60,000");
+            Console.WriteLine("===================================");
+
+            foreach(var e in employees)
+            {
+                if(e.Sales >= 60000)
+                {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void ProcessEmployee_30000_60000(Employee[] employees)
+        {
+            Console.WriteLine("Employees with Sales between 30,000 and 60,000 plus sales.");
+            Console.WriteLine("===================================");
+
+            foreach(var e in employees)
+            {
+                if((e.Sales >= 30000) && (e.Sales < 60000))
+                {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public void ProcessEmployee_30000L(Employee[] employees)
+        {
+            Console.WriteLine("Employees with Sales Less than 30,000.");
+            Console.WriteLine("===================================");
+
+            foreach(var e in employees)
+            {
+                if(e.Sales < 30000)
+                {Console.WriteLine($"{e.Id} , {e.Name} , {e.Gender} , {e.Sales}");}
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+
+}
