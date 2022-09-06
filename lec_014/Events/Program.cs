7eaaf5368ead0f54915d3a064c448d482915a57c
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-06 09:19:36 +0200

    Add files via upload

---
+++ lec_014/Events/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using StockClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stock amazon = new Stock("Amazon");
+        amazon.Price = 100;
+
+        Console.WriteLine($"Stock price before changing: {amazon.Price}");
+        amazon.ChangeStockPriceBy(0.05);  // cahnge by 5%
+        Console.WriteLine($"Stock price after changing by 5%: {amazon.Price}");
+
+    }
+}
---
+++ lec_014/StockClass/Stock.cs
@@ -0,0 +1,25 @@
+﻿namespace StockClass
+{
+    public class Stock
+    {
+        private string _name;
+        private double _price;
+
+        public string Name => this._name;
+        public double Price
+        {
+            get => this._price;
+            set => this._price = value;
+        }
+
+        public Stock(string name)
+        {
+            this._name = name;
+        }
+
+        public void ChangeStockPriceBy(double percent)
+        {
+            this._price += Math.Round(this._price * percent, 2);
+        }
+    }
+}
