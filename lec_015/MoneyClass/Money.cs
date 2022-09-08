19d0c6c3e215633f45fdc144ee3968aa19239754
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-08 10:36:55 +0200

    Add files via upload

---
+++ lec_015/MoneyClass/Money.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyClass
+{
+    public class Money
+    {
+        private decimal _amount;
+
+        public decimal Amount => this._amount;  //property to return the amount
+
+        // Constructor
+        public Money(decimal value)
+        {
+            this._amount = Math.Round(value, 2);
+        }
+    }
+}
---
+++ lec_015/OpOverLoading/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using MoneyClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Money m1 = new Money(10);
+        Money m2 = new Money(20);
+
+        Console.WriteLine($"Money m1 = {m1.Amount}, Money m2 = {m2.Amount}");
+
+        Console.WriteLine($"With ordinary sum operator: m1.amount + m2.amount = {m1.Amount+m2.Amount}");
+    }
+}
