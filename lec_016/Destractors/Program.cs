22cc2675868b3908fb60a3ff8f76bccff3fc4224
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-08 14:39:11 +0200

    Creating a destructor

---
+++ lec_016/Destractors/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using PersonClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person();
+        person.Name = "Jessy";
+    }
+}
---
+++ lec_016/PersonClass/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonClass
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person()
+        {
+            Console.WriteLine("This is the Person's class constructor.");
+        }
+        
+        ~Person()
+        {
+            Console.WriteLine("This is the Person's class destructor.");
+        }
+    }
+}
