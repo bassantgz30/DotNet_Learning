f29f19d68c84cb03611cda7459dd3de47f608ce0
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-08 14:54:32 +0200

    using  Garbage Collector method

--- lec_016/Destractors/Program.cs
+++ lec_016/Destractors/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 using PersonClass;
 
@@ -6,7 +6,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        var person = new Person();
-        person.Name = "Jessy";
+        MakeSomeGarbage();
+
+        Console.WriteLine($"Memory used before Garbage Collectio: {GC.GetTotalMemory(false):N0}");
+        
+        // collect garbage
+        GC.Collect();
+
+        Console.WriteLine($"Memory used after Garbage Collectio: {GC.GetTotalMemory(true):N0}");
+
+    }
+
+    static void MakeSomeGarbage()
+    {
+        Person p;
+
+        // crearing some garbage
+        for(int i = 0; i < 1000; i++)
+        {
+            p = new Person();
+        }
     }
 }
