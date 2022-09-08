f6906b00a1eb2f4557f7eeb5c8b2aea227670542
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-08 10:56:21 +0200

    test operator overloading

--- lec_015/OpOverLoading/Program.cs
+++ lec_015/OpOverLoading/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 using MoneyClass;
 
@@ -12,5 +12,7 @@ class Program
         Console.WriteLine($"Money m1 = {m1.Amount}, Money m2 = {m2.Amount}");
 
         Console.WriteLine($"With ordinary sum operator: m1.amount + m2.amount = {m1.Amount+m2.Amount}");
+
+        Console.WriteLine($"With Operator overloading: m1 + m2 = {(m1 + m2).Amount}");
     }
 }
