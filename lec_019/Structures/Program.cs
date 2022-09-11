e6efa04e863f46b9db653fc5fa0484eca34d5c55
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-11 11:05:37 +0200

    Built basic struct

--- lec_019/Structures/Program.cs
+++ lec_019/Structures/Program.cs
@@ -1,10 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        DigitalsizeBits bits = new DigitalsizeBits(9000000);
+
+        Console.WriteLine($"{bits.Bit}");
+        Console.WriteLine($"{bits.Byte}");
+        Console.WriteLine($"{bits.Kbyte}");
+        Console.WriteLine($"{bits.Mbyte}");
     }
+}
+
+struct DigitalsizeBits
+{
+    private long bit;
+
+    public DigitalsizeBits(long initailSize)
+    {
+        this.bit = initailSize;
+    }
+
+    // Constants for conversions
+    private const long BITSnBITS = 1;  // convert bits to bits
+    private const long BITSnBYTES = 8;  // convert bytes to bits ( 1 byte = 8 bits)
+    private const long BITSnKBYTES = 1024*8;  // Kbyte to bits
+    private const long BITSnMBYTES = 1024*1024*8; // Mbyte to bits
+
+    // Property to convrt the size into bits and return it
+    public string Bit => $"{bit} bit = {bit} bits";
+    public string Byte => $"{bit} bits = {bit/BITSnBYTES} Bytes";
+    public string Kbyte => $"{bit} bits = {(bit/BITSnKBYTES)} KB";
+    public string Mbyte => $"{bit} bits = {(bit/BITSnMBYTES)} MB";
 }
