02ce0e1b45518f8fe0d027506fa7efb90d8bedb2
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-12 08:59:51 +0200

    Simple Enumerator

---
+++ lec_020/Enumerators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MonthEnumerator;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{Month.Feb}");
+    }
+}
+
---
+++ lec_020/MonthEnumerator.cs
@@ -0,0 +1,23 @@
+/*
+    Simplw example for enumerator.
+    - default type is int
+*/
+
+namespace MonthEnumerator
+{
+    enum Month
+    {
+        Jan = 1,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+}
