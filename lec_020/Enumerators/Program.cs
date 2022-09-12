b6fdbd23a65b0da26e8fa2fa482e6ec96bc659b6
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-12 09:32:46 +0200

    try flag enum

--- lec_020/Enumerators/Program.cs
+++ lec_020/Enumerators/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 using MonthEnumerator;
+using DayEnumerator;
 
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Simple enumerator
         Console.WriteLine($"{(int)Month.Feb}");
+
+        // Flag enumerator
+        var day = (Day.Sat | Day.Sun);
+
+        if(day.HasFlag(Day.WeekEnds))
+        {
+            Console.WriteLine("Enjoy your weekend!");
+        }
+
     }
 }
 
