d4921eaef7b144d46a9a99610bad3af6d7161f8f
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-13 09:38:44 +0200

    Inheritance
    Basic classes before using inheritance

---
+++ lec_021/AnimalsClass.cs
@@ -0,0 +1,18 @@
+namespace AnimalsClass
+{
+    class Animal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Moving .....");
+        }
+    }
+
+    class Eagle
+    {
+        public void Move()
+        {
+            Console.WriteLine("Flying .....");
+        }
+    }
+}
---
+++ lec_021/Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using AnimalsClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
