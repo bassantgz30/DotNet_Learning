bde9996f0f400a87054ce9151dcc6073da220f05
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-18 10:11:31 +0200

    using method overload

---
+++ lec_023/Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        print(1);
+        print("Hello");
+        print('c');
+    }
+
+    static void print(int i) => Console.WriteLine(i);
+    static void print(string s) => Console.WriteLine(s);
+    static void print(char c) => Console.WriteLine(c);
+}
