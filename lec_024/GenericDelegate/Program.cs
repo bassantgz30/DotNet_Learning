515aa506e4f161c9199e5481e99b409d300ea440
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-19 09:27:03 +0200

    with generic delegate

--- lec_024/GenericDelegate/Program.cs
+++ lec_024/GenericDelegate/Program.cs
@@ -3,25 +3,39 @@ using System;
 
 class Program
 {
-    public delegate bool Filter(int n);
+    public delegate bool Filter<in T>(T n);     // Generic delegate
 
     static void Main(string[] args)
     {
         // Generic Interface
         IEnumerable<int> list1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8};
         Console.WriteLine($"Numbers less than 6: ");
-        PrintNumber(list1, n => n < 6);
+        PrintElement(list1, n => n < 6);
         Console.WriteLine($"Numbers grater than 5: ");
-        PrintNumber(list1, n => n > 5);
+        PrintElement(list1, n => n > 5);
         Console.WriteLine($"Even numbers: ");
-        PrintNumber(list1, n => n%2 == 0);
+        PrintElement(list1, n => n%2 == 0);
         Console.WriteLine($"Odd numbers: ");
-        PrintNumber(list1, n => n%2 == 1);
+        PrintElement(list1, n => n%2 == 1);
+
+
+        IEnumerable<double> list2 = new double[] {10.5, 12.45, 5.778, 6.01, 4.56, 8.89, 14.23, 1.9};
+        Console.WriteLine($"Numbers less than 7.5: ");
+        PrintElement(list2, n => n < 7.5);
+        Console.WriteLine($"Numbers grater than 5.8: ");
+        PrintElement(list2, n => n > 5.8);
+
+        
+        IEnumerable<string> list3 = new string[] {"abc", "klmn", "x", "hijop", "defg", "xx"};
+        Console.WriteLine($"String with length less than 4: ");
+        PrintElement(list3, s => s.Length < 4);
+        Console.WriteLine($"Sringd with length greater than or equal 4: ");
+        PrintElement(list3, s => s.Length >= 4);
     }
 
-    static void PrintNumber(IEnumerable<int> arr, Filter filter)
+    static void PrintElement<T>(IEnumerable<T> arr, Filter<T> filter)
     {
-        foreach (int i in arr)
+        foreach (var i in arr)
         {
             if(filter(i))
                 {Console.WriteLine(i);}
