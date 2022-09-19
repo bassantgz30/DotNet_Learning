eccad85b390c97b969ef1a3ca3b25ec888c08449
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-19 09:10:53 +0200

    with delegate (int)

--- lec_024/GenericDelegate/Program.cs
+++ lec_024/GenericDelegate/Program.cs
@@ -1,55 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 
 class Program
 {
+    public delegate bool Filter(int n);
+
     static void Main(string[] args)
     {
         // Generic Interface
         IEnumerable<int> list1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8};
         Console.WriteLine($"Numbers less than 6: ");
-        PrintLessThan6(list1);
+        PrintNumber(list1, n => n < 6);
         Console.WriteLine($"Numbers grater than 5: ");
-        PrintGreaterThan5(list1);
+        PrintNumber(list1, n => n > 5);
         Console.WriteLine($"Even numbers: ");
-        PrintEvenNumbers(list1);
+        PrintNumber(list1, n => n%2 == 0);
         Console.WriteLine($"Odd numbers: ");
-        PrintOddNumbers(list1);
+        PrintNumber(list1, n => n%2 == 1);
     }
 
-    static void PrintLessThan6(IEnumerable<int> arr)
+    static void PrintNumber(IEnumerable<int> arr, Filter filter)
     {
         foreach (int i in arr)
         {
-            if(i < 6)
+            if(filter(i))
                 {Console.WriteLine(i);}
         }
     }
 
-    static void PrintGreaterThan5(IEnumerable<int> arr)
-    {
-        foreach (int i in arr)
-        {
-            if(i > 5)
-                {Console.WriteLine(i);}
-        }
-    }
-
-    static void PrintEvenNumbers(IEnumerable<int> arr)
-    {
-        foreach (int i in arr)
-        {
-            if(i%2 == 0)
-                {Console.WriteLine(i);}
-        }
-    }
-
-    static void PrintOddNumbers(IEnumerable<int> arr)
-    {
-        foreach (int i in arr)
-        {
-            if(i%2 == 1)
-                {Console.WriteLine(i);}
-        }
-    }
 }
