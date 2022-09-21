160790f0747cbbdadff42df88328282f2118fcbf
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-21 09:01:17 +0200

    Basic
    compare objects using '=='

---
+++ lec_026/CSEnumeratorIterator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ProjectClasses;
+
+
+class Project
+{
+    static void Main(string[] args)
+    {
+        Employee e1 = new Employee {id=1504, name="Jina", salary=6500, department="testing"};
+        Employee e2 = new Employee {id=1504, name="Jina", salary=6500, department="testing"};
+
+        Console.WriteLine($"e1 is identical to e2? {e1 == e2}");  // Compare refrences i.e. memorey location
+
+        Employee e3 = e2;   // both point to the same memory location
+        Console.WriteLine($"e3 is identical to e2? {e3 == e2}");
+
+    }
+}
