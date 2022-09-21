9457737b6374743bc213d09e1c16171b5603c799
Bassant Gamal <44124521+bassantgz30@users.noreply.github.com>
2022-09-21 09:25:33 +0200

    overriding 'Equals()' method

--- lec_026/Employee.cs
+++ lec_026/Employee.cs
@@ -6,5 +6,15 @@ namespace ProjectClasses
         public string name { get; set; }
         public decimal salary { get; set; }
         public string department { get; set; }
+
+        public override bool Equals(object obj)
+        { 
+            if ((obj == null) || (!(obj is Employee))) return false;
+            var emp = obj as Employee;
+            return this.id == emp.id 
+                    && this.name == emp.name 
+                    && this.salary == emp.salary 
+                    && this.department == emp.department;
+        }
     }
 }
